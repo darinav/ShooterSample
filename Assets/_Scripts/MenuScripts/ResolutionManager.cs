ecbc2fc0c7b2e54de1e33d2984c185ced11a7ee5
Darina Voloshyna <23.04@mail.ru>
2015-09-12 02:32:39 +0300

    Main menu

---
+++ Assets/_Scripts/MenuScripts/ResolutionManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ResolutionManager : MonoBehaviour
+{    
+    public Slider ResolutionSlider;
+    int[] lowestres = { 960, 540 };
+    int[] lowres = { 1280, 720 };
+    int[] midres = { 1600, 900 };
+    int[] highres = { 1920, 1080 };
+    Resolution desirableResolution;
+    
+    public void OnTogglePress()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+    }
+
+    public void ValueChangeCheck()
+    {
+        if(ResolutionSlider.value == 0)
+        {
+            desirableResolution.width = lowestres[0];
+            desirableResolution.height = lowestres[1];
+        }
+        else if (ResolutionSlider.value == 1)
+        {
+            desirableResolution.width = lowres[0];
+            desirableResolution.height = lowres[1];
+        }
+        else if (ResolutionSlider.value == 2)
+        {
+            desirableResolution.width = midres[0];
+            desirableResolution.height = midres[1];
+        }
+        else if (ResolutionSlider.value == 3)
+        {
+            desirableResolution.width = highres[0];
+            desirableResolution.height = highres[1];
+        }
+    }        
+
+    public void OnApplyPress()
+    {
+        Screen.SetResolution(desirableResolution.width, desirableResolution.height, Screen.fullScreen);
+        UI_MainMenu.CurrentRes = Screen.currentResolution;
+    }
+}
---
+++ Assets/_Scripts/MenuScripts/UI_MainMenu.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UI_MainMenu : MonoBehaviour
+{
+    public static Resolution CurrentRes;
+    public Canvas MenuCanvas;
+    public GameObject MenuPanel;
+    public GameObject SettingsPanel;
+    public Button Play;
+    public Button Settings;
+    public Button Quit;
+
+    void Awake()
+    {
+        MenuCanvas = MenuCanvas.GetComponent<Canvas>();
+        MenuPanel = GameObject.Find("MenuPanel");
+        SettingsPanel = GameObject.Find("SettingsPanel");
+        Play = GetComponent<Button>();
+        Settings = GetComponent<Button>();
+        Quit = GetComponent<Button>();
+
+        CurrentRes = Screen.currentResolution;
+
+        SettingsPanel.SetActive(false);
+    }
+
+    public void OnPlayPress()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void OnSettingsPress()
+    {
+        SettingsPanel.SetActive(true);
+        MenuPanel.SetActive(false);
+
+    }
+
+    public void OnQuitPress()
+    {
+        Application.Quit();
+    }
+
+    public void OnBackPress()
+    {
+        SettingsPanel.SetActive(false);
+        MenuPanel.SetActive(true);
+    }
+}
